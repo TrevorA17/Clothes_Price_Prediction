1d534447afd8707400a74ef2681c8fbba584ecde
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-05-29 20:37:01 +0300

    Boostrapping

--- TrainingModel.R
+++ TrainingModel.R
@@ -19,3 +19,22 @@ test_data <- subset(clothes_data, split == FALSE)
 
 dim(train_data)
 dim(test_data)
+
+# Load necessary library
+library(boot)
+
+# Define function for bootstrapping
+bootstrap_func <- function(data, indices) {
+  sampled_data <- data[indices, ]
+  # Perform your analysis or modeling here
+  # For example, you can fit a linear model:
+  lm_model <- lm(Price ~ ., data = sampled_data)
+  # Then return the parameters you're interested in
+  return(coef(lm_model))
+}
+
+# Perform bootstrapping
+boot_results <- boot(data = clothes_data, statistic = bootstrap_func, R = 1000)
+
+# View bootstrap results
+print(boot_results)
